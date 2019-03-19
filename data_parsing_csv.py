493f482ed63b6e9e483108aa3506fefca000f16b
Anand Kumar <anandraz066@gmail.com>
2019-03-19 08:52:00 +0530

    Add files via upload
    >> data_parsing_csv.py
    This project is prototype of data parsing into csv file like start_timestamp and end_timestamp into csv file as realtime. Output: Enter the rec name: my_data (give csv filename) if gile is there in your directory it will append if not there it will create. Event: 1 Start Timestamp:11111111111111 End Timestamp:2222222222222

---
+++ data_parsing_csv.py
@@ -0,0 +1,55 @@
+import csv
+
+# take the input data from user and return data in form of list
+def data(i):
+    print("Event: ", i)
+
+    my_list= []  # created empty list
+
+    start_timestamp = int(input("Start Timestamp:"))
+    my_list.append(start_timestamp)
+
+    end_timestamp = int(input("End Timestamp:"))
+    my_list.append(end_timestamp)
+
+    return my_list
+
+# create csv, open csv and parsing the user data into csv
+def data_parse(my_list):
+
+    file_object= open(rec_name + '.csv', 'a', newline= '')
+
+    write_object= csv.writer(file_object)
+
+    write_object.writerow(my_list)
+
+# asking user to continue
+def user_input():
+    print("still you want to enter the data:")
+    check= int(input("Continue press 1 exit 0: "))
+
+    return check
+
+rec_name = str(input("Enter the rec name: "))
+
+i=1
+while(1):
+    my_list= data(i)
+    i+=1
+
+    data_parse(my_list)
+
+    print("***********************************************************************************")
+    check= user_input()
+
+    if check== 1:
+        continue
+    elif check==0:
+        exit(1)
+    else:
+        print("*************wrong enter*****************")
+        check= user_input()
+
+
+
+
