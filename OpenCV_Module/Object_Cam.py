c553b97408c285d24e9f1fa47c04352a4da9c014
anandraz <anandraz066@gmail.com>
2021-06-21 00:06:13 +0530

    Cam enable by using opencv

---
+++ OpenCV_Module/Object_Cam.py
@@ -0,0 +1,19 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(cap.isOpened()):
+    ret, frame1 = cap.read()
+    #ret, frame2 = cap.read()
+
+    #diff = cv2.absdiff(frame1, frame2)
+
+    if cv2.waitKey(10) == ord('q'):
+        break
+
+    #cv2.imshow('frame', diff) 
+    
+    #gray = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Frame", frame1)
+cap.release()
+cv2.destroyAllWindows()
