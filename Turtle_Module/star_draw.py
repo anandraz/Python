cfe72fa01ac6f7d98618c5b0a217975df704c040
anandraz <anandraz066@gmail.com>
2021-06-27 00:10:41 +0530

    This is start drawing...

---
+++ Turtle_Module/star_draw.py
@@ -0,0 +1,31 @@
+__author__ = "Anand Kumar"
+__copyright__ = "Copyright (C) 2021"
+__version__ = "1.0"
+
+"""
+Package Instalation: pip install turtle
+Source: https://docs.python.org/3/library/turtle.html#module-turtle
+"""
+
+from turtle import *
+
+title("Art with code")
+setup (width=800, height=600)
+bgcolor("white")
+screensize(100, 400)
+
+color('green', 'purple')
+
+begin_fill()
+while True:
+    forward(200)
+    left(170)
+    pensize(1) # This is pen draw line size 
+    circle(8) # Make circle with radius 8
+    speed(10) # Increase faster drawing 
+    if abs(pos()) < 1:
+        break
+end_fill()
+done()
+
+
